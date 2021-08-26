5256f5b8ff719b41d3e645549e15369ebc5fb515
Miao <miaot2@fr-s-ivg-histomics.ncifcrf.gov>
2021-08-26 11:55:46 -0400

    get image path

--- girder_archive/external/aperio_proxy.py
+++ girder_archive/external/aperio_proxy.py
@@ -8,16 +8,34 @@ class AperioProxy(object):
             logger = logging.getLogger(__name__)
         self.crl = pycurl.Curl()
         self.logger = logger
+        self.baseURL = 'http://129.43.165.161/'
         self.username = username
         self.password = password
+        self.baseLoc = '//at-s-is2.ncifcrf.gov/phl_scanscope/static'
+        self.mountLoc = '/mnt/phl_scanscope/static'
         self.crl.setopt(pycurl.USERPWD, username + ':' + password)
         self.crl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_BASIC)
+
     def getAnn(self, id):
         e = io.BytesIO()
-        self.crl.setopt(pycurl.URL, 'http://129.43.165.161/@'+id+'?GETANNOTATIONS')
+        self.crl.setopt(pycurl.URL, self.baseURL + '@' + id + '?GETANNOTATIONS')
         self.crl.setopt(pycurl.WRITEFUNCTION, e.write)
         self.crl.perform()
 
         htmlString = e.getvalue().decode('UTF-8')
 
         return htmlString
+
+    def getImagePath(self, id):
+        e = io.BytesIO()
+        self.crl.setopt(pycurl.URL, self.baseURL + '@' + id + '?GETPATH')
+        self.crl.setopt(pycurl.WRITEFUNCTION, e.write)
+        self.crl.perform()
+
+        imagePath = e.getvalue().decode('UTF-8')
+        imagePath = imagePath.replace('\\','/').replace(self.baseLoc, self.mountLoc)
+
+        return imagePath
+
+    def getImagePathsByRequestId(self, requestId):
+        pass
